50fffca6271ddc496ff9ce6bac4ced8c9194ee40
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-01-18 11:52:48 -0700

    Add files via upload

---
+++ CustomBlock/CustomBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBlockApp.swift
+//  CustomBlock
+//
+//  Created by Ty Irvine on 2020-11-12.
+//
+
+import SwiftUI
+
+@main
+struct CustomBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
